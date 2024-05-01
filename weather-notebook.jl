9043903a3ad448482188d035b6e4624ab9fac25d
Adrià Lisa Bou <105350856+AdriaCoding@users.noreply.github.com>
2024-05-01 11:01:15 +0200

    Add files via upload

--- weather-notebook.jl
+++ weather-notebook.jl
@@ -7,5 +7,9 @@ using InteractiveUtils
 # ╔═╡ 1e925f60-0797-11ef-024d-f96b36f01881
 a = 2+2
 
+# ╔═╡ e17a2d34-5199-4bf9-a09c-b9c0f84fb720
+a*2
+
 # ╔═╡ Cell order:
 # ╠═1e925f60-0797-11ef-024d-f96b36f01881
+# ╠═e17a2d34-5199-4bf9-a09c-b9c0f84fb720
