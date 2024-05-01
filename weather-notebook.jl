4a556730b1d61a16612f6d98aad9dae628c3e4c0
Adrià Lisa Bou <105350856+AdriaCoding@users.noreply.github.com>
2024-05-01 10:53:00 +0200

    Add files via upload

---
+++ weather-notebook.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.41
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 1e925f60-0797-11ef-024d-f96b36f01881
+a = 2+2
+
+# ╔═╡ Cell order:
+# ╠═1e925f60-0797-11ef-024d-f96b36f01881
