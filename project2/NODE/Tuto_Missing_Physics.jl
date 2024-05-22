c91a1929e87e906171aadf79825defc212917dc0
Adrià Lisa Bou <105350856+AdriaCoding@users.noreply.github.com>
2024-05-22 16:09:28 +0200

    HYBRID MODEL WORKS!

--- project2/NODE/Tuto_Missing_Physics.jl
+++ project2/NODE/Tuto_Missing_Physics.jl
@@ -1,7 +1,7 @@
 # SciML Tools
 using OrdinaryDiffEq, ModelingToolkit, DataDrivenDiffEq, SciMLSensitivity, DataDrivenSparse
 using Optimization, OptimizationOptimisers, OptimizationOptimJL
-
+using ComponentArrays
 # Standard Libraries
 using LinearAlgebra, Statistics
 
@@ -43,20 +43,22 @@ rbf(x) = exp.(-(x .^ 2))
 const U = Lux.Chain(Lux.Dense(2, 5, rbf), Lux.Dense(5, 5, rbf), Lux.Dense(5, 5, rbf),
               Lux.Dense(5, 2))
 # Get the initial parameters and state variables of the model
-p, st = Lux.setup(rng, U)
+p_nn, st = Lux.setup(rng, U)
+
 const _st = st
+p= ComponentArray(NN=p_nn, LV=rand(rng, Float32,4))
 
 # Define the hybrid model
-function ude_dynamics!(du, u, p, t, p_true)
-    û = U(u, p, _st)[1] # Network prediction
-    du[1] = p_true[1] * u[1] + û[1]
-    du[2] = -p_true[4] * u[2] + û[2]
+function ude_dynamics!(du, u, p, t)
+    û = U(u, p.NN, _st)[1] # Forward pass
+    α, β, γ, δ = p.LV
+    # Lokta-Volterra equations + ANN
+    du[1] = α*u[1] - β*u[1]*u[2] + û[1]
+    du[2] = γ*u[1]*u[2] - δ*u[2] + û[2]
 end
 
-# Closure with the known parameter
-nn_dynamics!(du, u, p, t) = ude_dynamics!(du, u, p, t, p_)
 # Define the problem
-prob_nn = ODEProblem(nn_dynamics!, Xₙ[:, 1], tspan, p)
+prob_nn = ODEProblem(ude_dynamics!, Xₙ[:, 1], tspan, p)
 
 function predict(θ, X = Xₙ[:, 1], T = t)
     _prob = remake(prob_nn, u0 = X, tspan = (T[1], T[end]), p = θ)
@@ -72,6 +74,8 @@ function loss(θ)
     mean(abs2, Xₙ .- X̂)
 end
 
+@time loss(p)
+
 losses = Float64[]
 
 callback = function (p, l)
@@ -86,20 +90,24 @@ adtype = Optimization.AutoZygote()
 optf = Optimization.OptimizationFunction((x, p) -> loss(x), adtype)
 optprob = Optimization.OptimizationProblem(optf, ComponentVector{Float64}(p))
 
-res1 = Optimization.solve(optprob, ADAM(), callback = callback, maxiters = 5000)
+import Base: real
+real(p::NamedTuple{T}) where T = 0f0
+
+
+res1 = Optimization.solve(optprob, ADAM(), callback = callback, maxiters = 1000)
 println("Training loss after $(length(losses)) iterations: $(losses[end])")
 
 optprob2 = Optimization.OptimizationProblem(optf, res1.u)
-res2 = Optimization.solve(optprob2, Optim.LBFGS(), callback = callback, maxiters = 5000)
+res2 = Optimization.solve(optprob2, Optim.LBFGS(), callback = callback, maxiters = 1000)
 println("Final training loss after $(length(losses)) iterations: $(losses[end])")
 
 # Rename the best candidate
 p_trained = res2.u
 
 # Plot the losses
-pl_losses = plot(1:5000, losses[1:5000], yaxis = :log10, xaxis = :log10,
+pl_losses = plot(1:1000, losses[1:1000], yaxis = :log10, xaxis = :log10,
                  xlabel = "Iterations", ylabel = "Loss", label = "ADAM", color = :blue)
-plot!(5001:length(losses), losses[5001:end], yaxis = :log10, xaxis = :log10,
+plot!(1001:length(losses), losses[1001:end], yaxis = :log10, xaxis = :log10,
       xlabel = "Iterations", ylabel = "Loss", label = "BFGS", color = :red)
 
       ## Analysis of the trained network
@@ -110,3 +118,17 @@ X̂ = predict(p_trained, Xₙ[:, 1], ts)
 pl_trajectory = plot(ts, transpose(X̂), xlabel = "t", ylabel = "x(t), y(t)", color = :red,
                      label = ["UDE Approximation" nothing])
 scatter!(solution.t, transpose(Xₙ), color = :black, label = ["Measurements" nothing])
+
+# See behaviour of out modle in the future
+tspan_future = (0.0, 40.0)
+prob_future = ODEProblem(lotka!, u0, tspan_future, p_)
+solution_f = solve(prob_future, Vern7(), abstol = 1e-12, reltol = 1e-12, saveat = 0.25)
+ts_f = first(solution_f.t):(mean(diff(solution_f.t)) / 2):last(solution_f.t)
+X̂_f = predict(p_trained, Xₙ[:, 1], ts_f)
+pl_trajectory = plot(ts_f, transpose(X̂_f), xlabel = "t", ylabel = "x(t), y(t)", color = :red,
+                     label = ["UDE Approximation" nothing])
+plot!(solution_f, alpha = 0.75, color = :black, label = ["True Data" nothing])
+
+println("Our real parameters and the ones obtained by the NN are quite unsimilar")
+println("Real  -> ", round.(p_, digits=1))
+println("Model -> ", round.(p_trained[:LV]', digits=2))
