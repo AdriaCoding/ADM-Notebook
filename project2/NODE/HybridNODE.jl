c013783a24e30be30d3d72633d2f78d076618723
Adrià Lisa Bou <105350856+AdriaCoding@users.noreply.github.com>
2024-05-23 00:20:15 +0200

    Stifness problems

--- project2/NODE/HybridNODE.jl
+++ project2/NODE/HybridNODE.jl
@@ -13,17 +13,19 @@ train_years = rawdata.year[1:train_size]
 
 # Normalize data
 scale = eachcol(df) .|> maximum |> transpose |> Array
-normalized_data = Array(df_train./scale)'
+const normalized_data = Array(df_train./scale)'
 normalized_data' .* scale
 
 #Display our data
 dataplot = scatter(train_years, normalized_data[1,:], label="Hares", color="blue", lw=2)
 scatter!(dataplot, train_years, normalized_data[2,:], label="Lynx", color="red", lw=2)
-    ## Problem conditions
-rng = Xoshiro(1)
+
+## Problem conditions
+rng = Xoshiro(0)
 u0 = normalized_data[:,1]
 tspan = Float32.((0.0, train_size-1))
 t = range(tspan[1], tspan[2], length=train_size) |> Array
+
 
 ## Define the network
 # Gaussian RBF as activation
@@ -35,8 +37,8 @@ U = Chain(
     )
 p_nn, state = Lux.setup(rng, U)
 const st = state
-# ps = [α, β, δ, γ; Network parameters]
-p= ComponentArray(NN=p_nn, LV=rand(rng, Float32,4))
+# p.LV = [α, β, δ, γ]
+p = ComponentArray(NN=p_nn, LV=rand(rng, Float32,4))
 
 # Define the hybrid model
 function ude_dynamics!(du, u, p, t)
@@ -46,25 +48,32 @@ function ude_dynamics!(du, u, p, t)
     du[1] = α*u[1] - β*u[1]*u[2] + û[1]
     du[2] = γ*u[1]*u[2] - δ*u[2] + û[2]
 end
-# Define the problem
 prob_nn = ODEProblem(ude_dynamics!, u0, tspan, p)
 
 initial_sol = solve(prob_nn, Rosenbrock23(), saveat = t)
-function predict(θ, u0=u0, T = t)
+
+function predict(θ; ODEalg = AutoTsit5(Rodas5()), u0=u0, T = t)
     _prob = remake(prob_nn, u0 = u0 , tspan = (T[1], T[end]), p = θ)
-    Array(solve(_prob, Rosenbrock23(), saveat = T,
-    abstol = 1e-6, reltol = 1e-6,
+    Array(solve(_prob, ODEalg, saveat = T,
+    abstol = 1e-2, reltol = 1e-2,
     sensealg=QuadratureAdjoint(autojacvec=ReverseDiffVJP(true))))
 end
 
-using BenchmarkTools
-@btime predd = predict(p)
+# Test the speed of several stiff ODE solvers.
+algs = [Rosenbrock23(), Rodas4(), Rodas5(), AutoTsit5(Rosenbrock23()),  AutoTsit5(Rodas4()), AutoTsit5(Rodas5())];
+# To see reliable reults, run this line more than once.
+[@elapsed predict(p; ODEalg= alg) for alg in algs]'
 
+# Re-use the plot of the training data to paint new predicitons on top of it.
+# Set up an increased time span to see the predictions more smoothly.
+resolution = 8
+t8 = range(tspan[1], tspan[2], length=train_size*resolution) |> Array
+plot_years = range(rawdata.year[1], rawdata.year[train_size], length=train_size*resolution)
 paint(p, plt = dataplot) = begin
-    pred = predict(p)
-    predplot = plot(plt, train_years, pred[1,:], label="Hares (pred)",
-         color="blue", lw=2, ls=:dash, ylims=(-0.1,1.1))
-    plot!(train_years, pred[2,:], label="Lynx (pred)", color="red", lw=2, ls=:dash)
+    pred = predict(p; T=t8)
+    predplot = plot(plt, plot_years, pred[1,:], label="Hares (pred)",
+         color="blue", ylims=(-0.1,1.1))
+    plot!(plot_years, pred[2,:], label="Lynx (pred)", color="red")
     display(predplot)
 end
 
@@ -75,37 +84,59 @@ function loss(p)
     return sum(abs2, normalized_data .- pred)
 end
 
-@time loss(p)
 losses = Float32[]
 
-callback = function (p, l; doplot=true)
-    push!(losses, l)
-    if length(losses) % 100 == 0
-        println("Current loss after $(length(losses)) iterations: $(losses[end])")
+callback = function (opt_state, l; doplot=true)
+    if opt_state.iter % 100 == 0
+        println("Current loss after $(opt_state.iter) iterations: $(l)")
+    end
+    if opt_state.iter % 10 == 0
+        push!(losses, l)
         if doplot
-            paint(p.u)
+            p = opt_state.u
+            paint(p)
         end
     end
     return false
 end
 
 # train this model!!
+maxiters = 5000
+
 adtype = Optimization.AutoZygote()
 optf = Optimization.OptimizationFunction((x, p) -> loss(x), adtype)
 optprob = Optimization.OptimizationProblem(optf, p)
-res1 = Optimization.solve(optprob, ADAM(0.1), callback = callback, maxiters = 1000)
+res1 = Optimization.solve(optprob, ADAM(0.1), callback = callback, maxiters = maxiters)
 println("Training loss after $(length(losses)) iterations: $(losses[end])")
 
 optprob2 = Optimization.OptimizationProblem(optf, res1.u)
-res2 = Optimization.solve(optprob2, Optim.LBFGS(), callback = callback, maxiters = 1000)
+res2 = Optimization.solve(optprob2, Optim.LBFGS(), callback = callback, maxiters = maxiters)
 println("Final training loss after $(length(losses)) iterations: $(losses[end])")
 
-result_neuralode = Optimization.solve(optprob, opt,
-    maxiters = 40)
+# Set final value for the trained parameters
 
-optprob2 = remake(optprob; u0 = result_neuralode.u)
-callback(result_neuralode.u, loss(result_neuralode.u)...; doplot = true)
-result_neuralode2 = Optimization.solve(optprob2, Optim.BFGS(; initial_stepnorm = 0.01);
-    callback, allow_f_increases = false)
+p_trained = res2.u
 
-callback(result_neuralode2.u, loss(result_neuralode2.u)...; doplot = true)
+#Compare with test data 
+test_size = size(df, 1)
+begin
+    # First, plot the data 
+    test_data = df ./ scale |> Array |> transpose
+    scatter(rawdata.year, test_data[1,:], label="Hares", lw=2)
+    scatter!(rawdata.year, test_data[2,:], label="Lynx", lw=2)
+
+    # Make new predictions 
+    test_t = Array(range(0.0f0, test_size-1 |> Float32, length=test_size*resolution))
+    test_years = test_t .+ rawdata.year[1] 
+    test_pred = predict(p_trained, AutoTsit5(Rodas5()), test_data[:,1], test_t)
+    sep = length(plot_years)
+    test_pred[1,1:sep]
+    plot!(test_years[1:sep], test_pred[1,1:sep], label="Hares (pred)", color="blue", lw=1)
+    plot!(test_years[1:sep], test_pred[2,1:sep], label="Lynx (pred)", color="red", lw=1)
+
+    test_pred[1,sep:end]
+    test_years[sep:end]
+    plot!(test_years[sep:end], test_pred[1,sep:end], label=nothing, color="blue", lw=1, linestyle=:dash)
+    plot!(test_years[sep:end], test_pred[2,sep:end], label=nothing, color="red", lw=1, linestyle=:dash)
+    title!("Hares and Lynx population")
+end
